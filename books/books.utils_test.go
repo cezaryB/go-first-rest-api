6eb08c2a71ecd4bf6b85b2a33fc92c9cad166c59
cezaryB <czarekburzykowski@gmail.com>
2023-02-08 11:59:44 +0100

    WIP: findBookById unit tests

--- books/books.utils_test.go
+++ books/books.utils_test.go
@@ -2,6 +2,7 @@ package books
 
 import (
 	"errors"
+	"reflect"
 	"testing"
 )
 
@@ -30,7 +31,38 @@ func TestTransformGenre(t *testing.T) {
 		}
 
 		if err != nil && err.Error() != table.e.Error() {
-			t.Errorf("Error message was incorrect, expected %v, got %v", err.Error(), table.e.Error())
+			t.Errorf("Error message was incorrect, expected %v, got %v", table.e.Error(), err.Error())
 		}
 	}
+}
+
+func TestFindBookById(t *testing.T) {
+	var e error
+	var eBook book
+	books := []book{
+		{ Id: 1, Title: "Arkham horror", Author: "H.P.Lovecraft", Genre: horror },
+		{ Id: 2, Title: "Lenore's shadow", Author: "E.A.Poe", Genre: horror },
+	}
+	tables := []struct{
+		x string
+		b book
+		e error
+	}{
+		{ "1", books[0], e},
+		{ "2", books[1], e},
+		{ "3", eBook, errors.New("Book not found")},
+	}
+
+	for _, table := range tables {
+		b, err := findBookById(table.x, books)
+
+		if err == nil && !reflect.DeepEqual(b, table.b) {
+			t.Errorf("Retuned book was incorrect, expected %v, got %v", table.b, b)
+		}
+
+		if err != nil && err.Error() != table.e.Error() {
+			t.Errorf("Error message was incorrect, expected %v, got %v", table.e.Error(), err.Error())
+		}
+	}
+
 }
